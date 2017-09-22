b052f410b658b71d28560dec935ce2764a0b37c7
Dan Giaime <dsg6874@g.rit.edu>
2017-09-21 22:22:42 -0400

    Start dijkstras

---
+++ Assets/Scripts/Algorithms.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Algorithms : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public List<Node> Dijkstras(Graph graph)
+    {
+        List<Node> solution = new List<Node>();
+        Node start = graph.StartNode;
+        Node target = graph.TargetNode;
+
+
+        return solution;
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Graph {
-	List<Node> vertices;
+	private List<Node> vertices;
+    private Node startNode;
+    private Node targetNode;
 
 	public Graph() {
 		vertices = new List<Node> ();
@@ -16,4 +18,16 @@ public class Graph {
 	public void addNode(Node n) {
 		vertices.Add (n);
 	}
+
+    public Node StartNode
+    {
+        get { return this.startNode; }
+        set { this.startNode = value; }
+    }
+
+    public Node TargetNode
+    {
+        get { return this.targetNode; }
+        set { this.targetNode = value; }
+    }
 }
