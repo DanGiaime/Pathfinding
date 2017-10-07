22cbee227c7d733080b283a9ced48cf2718c1dbd
DanGiaime <dsg6874@g.rit.edu>
2017-10-06 22:04:39 -0400

    IPriorityQueue

--- Assets/Scripts/PriorityQueue.cs
+++ Assets/Scripts/PriorityQueue.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IPriorityQueue<T, V> {
+public interface IPriorityQueue<K, V> {
 
 	/// <summary>
 	/// Gets the next node from PriorityQueue and removes it 
 	/// </summary>
-	KeyValuePair<T, V> Next();
+	KeyValuePair<K, V> Next();
 
 	/// <summary>
 	/// Adds a node to the PriorityQueue.
 	/// </summary>
 	/// <param name="n">Node to add</param>
-	void Add (T n, V dist);
+	void Add (K n, V dist);
 
 	/// <summary>
 	/// Return true if the key exists within the structure
 	/// </summary>
 	/// <returns><c>true</c>, if key was contained, <c>false</c> otherwise.</returns>
 	/// <param name="n">N.</param>
-	bool ContainsKey(T n);
+	bool ContainsKey(K n);
+
+    V this[K n] {
+        set;
+        get;
+    }
+
 }
